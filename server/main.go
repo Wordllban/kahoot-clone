9d96731fe567d77ea49de3c61ce2eb6f21db5ccf
Wordllban <mininkov3228@gmail.com>
2024-11-13 22:11:45 +0200

    add cors on server

--- server/main.go
+++ server/main.go
@@ -1,16 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
 	app := fiber.New()
+	// needed only for development
+	// we will run our application on the same server in prod
+	app.Use(cors.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.Get("/", index)
 	app.Get("/api/v1/quizzes", getQuizzes)
 
 	app.Listen(":3000")
+}
+
+func index(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
 }
 
 func getQuizzes(c *fiber.Ctx) error {
