4f45af4d5d1df0444df7c5f3e40305617c784e4c
Wordllban <mininkov3228@gmail.com>
2024-11-13 21:59:51 +0200

    basic quizzes endpoint

--- server/main.go
+++ server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	fmt.Println("hello world!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Get("/api/v1/quizzes", getQuizzes)
+
+	app.Listen(":3000")
+}
+
+func getQuizzes(c *fiber.Ctx) error {
+	list := []map[string]any{
+		{
+			"test": 123,
+		},
+	}
+	return c.JSON(list)
 }
