707e5c7055fee434e0343e1d3f7338834b37d4f5
Wordllban <mininkov3228@gmail.com>
2024-11-15 21:24:34 +0200

    ui: update button component

--- ui/src/lib/Button.svelte
+++ ui/src/lib/Button.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   interface Props {
     children: any;
+    onclick?: () => void;
   }
-  let { children }: Props = $props();
+
+  let { children, onclick }: Props = $props();
 </script>
 
-<button class="bg-blue-500 hover:bg-blue-600 text-white rounded-md px-4 py-2">
+<button
+  {onclick}
+  class="bg-blue-500 hover:bg-blue-600 text-white rounded-md px-4 py-2">
   {@render children?.()}
 </button>
